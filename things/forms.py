9bf723d11fc63a968a9d11fe55d4bde1b185b60d
sdharren <sdharren@yahoo.co.uk>
2022-11-18 21:12:16 +0000

    changed labels and get widgets

--- things/forms.py
+++ things/forms.py
@@ -4,9 +4,7 @@ from .models import Thing
 
 # Create your forms here.
 class ThingForm(forms.ModelForm):
-    class Meta:
-        model = Thing
-        fields = ["name"]
     
-    description = forms.CharField(widget=forms.Textarea)
-    quantity = forms.NumberInput()
+    name = forms.CharField(label="name", max_length=35)
+    description = forms.CharField(label = "description", widget=forms.Textarea, max_length=120)
+    quantity = forms.CharField(label="quantity", widget=forms.NumberInput)
