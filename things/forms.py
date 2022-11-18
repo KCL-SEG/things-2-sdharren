7bb144fcbe617b96a37e52420c5a4c9bfc5f7b5f
sdharren <sdharren@yahoo.co.uk>
2022-11-18 21:46:44 +0000

    added model to form

--- things/forms.py
+++ things/forms.py
@@ -4,7 +4,8 @@ from .models import Thing
 
 # Create your forms here.
 class ThingForm(forms.ModelForm):
-    
+    class Meta:
+        model = Thing
     name = forms.CharField(label="name", max_length=35)
     description = forms.CharField(label = "description", widget=forms.Textarea, max_length=120)
     quantity = forms.CharField(label="quantity", widget=forms.NumberInput)
