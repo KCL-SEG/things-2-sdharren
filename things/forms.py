11af7aa5fbdeaf1ed44a2de55c747e550fcae32b
sdharren <sdharren@yahoo.co.uk>
2022-11-18 20:55:16 +0000

    added signupform

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,13 @@
 """Forms of the project."""
+from django import forms
+from .models import Thing
 
 # Create your forms here.
+class SignUpForm(forms.ModelForm):
+    class Meta:
+        model = Thing
+        fields = ["name"]
+    
+    description = forms.Textarea()
+    quantity = forms.NumberInput()
+    
